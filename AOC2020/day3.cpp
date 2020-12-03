c60e5f251c8c97100e9412678ed6d1b95864a93d
jaapjong <J.deJong-18@student.tudelft.nl>
2020-12-03 10:29:57 +0100

    Improve solution for day 3

--- AOC2020/day3.cpp
+++ AOC2020/day3.cpp
@@ -1,7 +1,7 @@
 #include "day3.h"
 #include <tuple>
 
-void day3_1() {
+std::vector<std::vector<bool>> getInput() {
 	std::ifstream file("input3.txt");
 	std::vector<std::vector<bool>> input;
 	for (std::string entry; std::getline(file, entry);) {
@@ -11,21 +11,12 @@ void day3_1() {
 		}
 		input.push_back(line_input);
 	}
-	//printf("input height: %i\ninput width: %i\n", input.size(), input[input.size() - 1].size());
-
-	int input_width = input[0].size();
-	int j = 3;
-	int answer = 0;
-	for (int i = 1; i < input.size(); i++) {
-		if (input[i][j % input_width]) {
-			answer++;
-		}
-		j += 3;
-	}
-	printf("Answer: %i", answer);
+	return input;
 }
 
-unsigned long long count(int x, int y, std::vector<std::vector<bool>> input) {
+std::vector<std::vector<bool>> input = getInput();
+
+long long count(int x, int y) {
 	unsigned long long answer = 0;
 	int input_width = input[0].size();
 	int j = x;
@@ -38,23 +29,25 @@ unsigned long long count(int x, int y, std::vector<std::vector<bool>> input) {
 	return answer;
 }
 
+void day3_1() {
+	auto start = std::chrono::high_resolution_clock::now();
+	
+	long long answer = count(3, 1);
+
+	auto stop = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double, std::milli> time = stop - start;
+	printf("==========\nPART 1\nAnswer:        %lld\nCalculated in: %f ms\n==========", answer, time.count());
+}
+
 void day3_2() {
-	std::ifstream file("input3.txt");
-	std::vector<std::vector<bool>> input;
-	for (std::string entry; std::getline(file, entry);) {
-		std::vector<bool> line_input;
-		for (char c : entry) {
-			line_input.push_back(c == '#');
-		}
-		input.push_back(line_input);
-	}
-	std::tuple<int, int> directions[5] = {
-		std::tuple<int, int>(1, 1),
-		std::tuple<int, int>(3, 1),
-		std::tuple<int, int>(5, 1),
-		std::tuple<int, int>(7, 1),
-		std::tuple<int, int>(1, 2)
-	};
-	unsigned long long answer = count(1, 1, input) * count(3, 1, input) * count(5, 1, input) * count(7, 1, input) * count(1, 2, input);
-	printf("Answer: %llu\n", answer);
+	auto start = std::chrono::high_resolution_clock::now();
+
+	long long answer = count(1, 1) * count(3, 1) * count(5, 1) * count(7, 1) * count(1, 2);
+
+	auto stop = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double, std::milli> time = stop - start;
+	printf("==========\nPART 2\nAnswer:        %lld\nCalculated in: %f ms\n==========", answer, time.count());
+
 }
