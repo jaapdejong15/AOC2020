6d22e2cb99b1f95609638eafd6342ad9fe587dff
jaapjong <J.deJong-18@student.tudelft.nl>
2020-12-05 09:56:56 +0100

    Add initial solution to day 5 part 2

--- AOC2020/day5.cpp
+++ AOC2020/day5.cpp
@@ -29,3 +29,42 @@ void day5_1() {
 	}
 	printf("Max seat id: %d\n", max_seat_id);
 }
+
+void day5_2() {
+	std::ifstream file("input5.txt");
+	std::vector<std::string> lines;
+	for (std::string line; std::getline(file, line);) {
+		lines.push_back(line);
+	}
+
+	std::vector<int> seat_ids;
+	for (std::string line : lines) {
+		int row = 0;
+		int column = 0;
+		for (int i = 0; i < line.size(); i++) {
+			switch (line[i]) {
+			case 'B':
+				row += 64 >> i;
+				break;
+			case 'R':
+				column += 4 >> i - 7;
+				break;
+			default:
+				break;
+			}
+		}
+		seat_ids.push_back(row * 8 + column);
+	}
+	std::sort(seat_ids.begin(), seat_ids.end());
+
+	int last_id = -3;
+	int answer = 0;
+	for (int id : seat_ids) {
+		if (id - last_id == 2) {
+			answer = id - 1;
+			break;
+		}
+		last_id = id;
+	}
+	printf("Answer %d\n", answer);
+}
