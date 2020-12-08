e52307e85ccfd4c1aa4d06f84df0311351c47962
jaapjong <J.deJong-18@student.tudelft.nl>
2020-12-08 09:17:04 +0100

    Day 8 part 2 solution

--- AOC2020/day8.cpp
+++ AOC2020/day8.cpp
@@ -1,6 +1,7 @@
 #include "day8.h"
 #include <unordered_set>
 #include <map>
+#include <tuple>
 
 enum class instruction {
 	nop,
@@ -31,7 +32,6 @@ void day8_1() {
 	int current_line = 0;
 	int accumulator = 0;
 	std::unordered_set<int> lines_visited;
-	lines_visited.insert(0);
 	int previous_size = 0;
 	while (true) {
 		lines_visited.insert(current_line);
@@ -58,6 +58,78 @@ void day8_1() {
 	std::cout << "ANSWER: " << accumulator << std::endl;
 }
 
-void day8_2() {
+std::tuple<bool, int> run(std::vector <std::tuple<instruction, int>> program) {
+	int current_line = 0;
+	int accumulator = 0;
+	std::unordered_set<int> lines_visited;
+	int previous_size = 0;
+	bool terminated = true;
+	while (current_line < program.size()) {
+		lines_visited.insert(current_line);
+		if (lines_visited.size() == previous_size) {
+			terminated = false;
+			break;
+		}
+		previous_size = lines_visited.size();
+		std::tuple<instruction, int> line = program[current_line];
+		instruction x = std::get<0>(line);
+		int argument = std::get<1>(line);
+		switch (x) {
+		case instruction::nop:
+			current_line++;
+			break;
+		case instruction::acc:
+			accumulator += argument;
+			current_line++;
+			break;
+		case instruction::jmp:
+			current_line += argument;
+			break;
+		}
+	}
+	return std::make_tuple(terminated, accumulator);
+}
 
+void day8_2() {
+	initialize8();
+	std::vector<std::string> input = getInput8();
+
+	std::vector<std::tuple<instruction, int>> program;
+	for (std::string line : input) {
+		instruction x = instruction_map[line.substr(0, 3)];
+		int argument = std::stoi(line.substr(4, 10));
+		program.push_back(std::make_tuple(x, argument));
+	}
+
+	int answer = 0;
+	std::tuple<bool, int> results;
+	for (int i = 0; i < program.size(); i++) {
+		std::tuple<instruction, int> line = program[i];
+		instruction x = std::get<0>(line);
+		int argument = std::get<1>(line);
+		switch (x) {
+		case instruction::nop:
+			program[i] = std::make_tuple(instruction::jmp, argument);
+			results = run(program);
+
+			if (std::get<0>(results)) {
+				answer = std::get<1>(results);
+			}
+			program[i] = std::make_tuple(instruction::nop, argument);
+			
+			break;
+		case instruction::jmp:
+			program[i] = std::make_tuple(instruction::nop, argument);
+			results = run(program);
+			if (std::get<0>(results)) {
+				answer = std::get<1>(results);
+			}
+			program[i] = std::make_tuple(instruction::jmp, argument);
+			break;
+		default:
+			break;
+		}
+	}
+
+	std::cout << "ANSWER: " << answer << std::endl;
 }
