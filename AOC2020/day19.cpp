49d44a27362890b2f4deeb0ebbaacec68bda36a5
jaapjong <J.deJong-18@student.tudelft.nl>
2020-12-19 14:46:34 +0100

    Improve runtimes for day 19

--- AOC2020/day19.cpp
+++ AOC2020/day19.cpp
@@ -2,21 +2,27 @@
 #include <unordered_map>
 
 bool match(std::string message, std::unordered_map<int, std::vector<std::string>>& rules, std::unordered_map<int, int>& rule_lengths, int ruleNr = 0) {
-	std::vector<std::string> rule = rules[ruleNr];
 	if (rule_lengths[ruleNr] != message.size()) return false;
-	if (rule[0][0] == '\"') return message[0] == rule[0][1];
+	if (rules[ruleNr][0][0] == '\"') return message[0] == rules[ruleNr][0][1];
 	int messagePointer = 0;
 	bool correct = true;
-	for (std::string x : rule) {
+	bool second_part = false;
+	int nxtRule;
+	int size;
+	for (std::string x : rules[ruleNr]) {
 		if (x[0] == '|') {
 			if (correct) break;
 			correct = true;
+			second_part = true;
 			messagePointer = 0;
 		}
 		else {
-			int nxtRule = std::stoi(x);
-			int size = rule_lengths[nxtRule];
-			if (!match(message.substr(messagePointer, size), rules, rule_lengths, nxtRule)) correct = false;
+			nxtRule = std::stoi(x);
+			size = rule_lengths[nxtRule];
+			if (!match(message.substr(messagePointer, size), rules, rule_lengths, nxtRule)) {
+				correct = false;
+				if (second_part) break;
+			}
 			messagePointer += size;
 		}
 	}
@@ -24,31 +30,31 @@ bool match(std::string message, std::unordered_map<int, std::vector<std::string>
 }
 
 bool match2(std::string message, std::unordered_map<int, std::vector<std::string>>& rules, std::unordered_map<int, int>& rule_lengths, int ruleNr = 0) {
+	int length42 = rule_lengths[42];
+	int length31 = rule_lengths[31];
 	if (ruleNr == 0) {
 		for (int i = 1; i < message.size() - 1; i++) {
-			if (match2(message.substr(i), rules, rule_lengths, 11) && match2(message.substr(0, i), rules, rule_lengths, 8)) return true;
+			if (i % length42 == 0 && (message.size() - i) % (length42 + length31) == 0 && match2(message.substr(i), rules, rule_lengths, 11) && match2(message.substr(0, i), rules, rule_lengths, 8)) return true;
 		}
 		return false;
 	}
 	else if (ruleNr == 8) {
-		if (message.size() % rule_lengths[42] != 0) return false;
 		int messagePointer = 0;
 		while (messagePointer < message.size()) {
-			if (!match(message.substr(messagePointer, rule_lengths[42]), rules, rule_lengths, 42)) {
+			if (!match(message.substr(messagePointer, length42), rules, rule_lengths, 42)) {
 				return false;
 			}
-			messagePointer += rule_lengths[42];
+			messagePointer += length42;
 		}
 		return true;
 	} 
 	else if (ruleNr == 11) {
-		if (message.size() % (rule_lengths[42] + rule_lengths[31]) != 0) return false;
 		int lowerPointer = 0;
 		int upperPointer = message.size();
 		while (lowerPointer < upperPointer) {
-			lowerPointer += rule_lengths[42];
-			upperPointer -= rule_lengths[31];
-			if (!match(message.substr(lowerPointer - rule_lengths[42], rule_lengths[42]), rules, rule_lengths, 42) || !match(message.substr(upperPointer, rule_lengths[31]), rules, rule_lengths, 31)) {
+			lowerPointer += length42;
+			upperPointer -= length31;
+			if (!match(message.substr(lowerPointer - length42, length42), rules, rule_lengths, 42) || !match(message.substr(upperPointer, length31), rules, rule_lengths, 31)) {
 				return false;
 			}
 		}
@@ -65,7 +71,7 @@ void find_rule_lengths(std::unordered_map<int, std::vector<std::string>>& rules,
 	for (std::string x : rules[rule]) {
 		if (x.size() != 0 && x[0] != '|') {
 			int ruleNr = std::stoi(x);
-			find_rule_lengths(rules, rule_lengths, ruleNr);
+			if (rule_lengths.find(ruleNr) == rule_lengths.end()) find_rule_lengths(rules, rule_lengths, ruleNr);
 			ruleNrs.push_back(ruleNr);
 		}
 		else ruleNrs.clear();
